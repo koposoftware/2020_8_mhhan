f84cb78937b326d9b675c09f560c2273733e11ee
miheeee <answer_lm@naver.com>
2020-09-29 17:18:19 +0900

    Update SmsSender.java

--- src/main/java/kr/ac/kopo/SmsSender.java
+++ src/main/java/kr/ac/kopo/SmsSender.java
@@ -13,9 +13,9 @@ import kr.ac.kopo.participant.vo.ParticipantVO;
 public class SmsSender {
  // Find your Account Sid and Auth Token at twilio.com/console
 	 public static final String ACCOUNT_SID =
-	         "AC01e8bc10bf65a6272002c0d1d48cc9bf";
+	         "";
 	 public static final String AUTH_TOKEN =
-	         "abf70692cf7a37c2c68b6eb1ec9ee370";
+	         "";
 	 
 //	 public static void main(String[] args) {
 //	     Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
